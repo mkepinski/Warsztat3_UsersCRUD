18c2b86a56cbdccee9c2855a723830489f1008a7
Michał Kępiński <kepinski.poznan@gmail.com>
2021-03-02 20:26:29 +0100

    connection set up, index.jsp with styles ready

---
+++ src/main/java/project/dao/UserDao.java
@@ -0,0 +1,109 @@
+package project.dao;
+
+import project.model.User;
+import project.util.DbUtil;
+
+import java.sql.*;
+import java.util.Arrays;
+
+public class UserDao {
+    private static final String CREATE_USER_QUERY = "INSERT INTO users (username, email, password) VALUES (?, ?, ?)";
+    private static final String READ_USER_QUERY = "SELECT * FROM users WHERE id = ?";
+    private static final String UPDATE_USER_QUERY = "UPDATE users SET username=?, email=?, password=? WHERE id = ?";
+    private static final String DELETE_USER_QUERY = "DELETE FROM users WHERE id = ?";
+    private static final String FIND_ALL_QUERY = "SELECT * FROM users WHERE id > ?";
+
+    public User create(User user) {
+        try(Connection connection = DbUtil.getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(CREATE_USER_QUERY, Statement.RETURN_GENERATED_KEYS);
+            statement.setString(1, user.getUserName());
+            statement.setString(2, user.getEmail());
+            statement.setString(3, user.getPassword());
+            statement.executeUpdate();
+
+            ResultSet rs = statement.getGeneratedKeys();
+            if(rs.next()) {
+                user.setId(rs.getInt(1));
+            }
+            return user;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public User read(int userId) {
+        User user = null;
+        try(Connection connection = DbUtil.getConnection()) {
+            user = new User();
+            PreparedStatement preparedStatement = connection.prepareStatement(READ_USER_QUERY);
+            preparedStatement.setInt(1, userId);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while(resultSet.next()) {
+                user.setUserName(resultSet.getString("username"));
+                user.setEmail(resultSet.getString("email"));
+                user.setPassword(resultSet.getString("password"));
+            }
+            return user;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return user;
+        }
+    }
+
+    public void update(User user) {
+        try(Connection connection = DbUtil.getConnection()) {
+            PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_USER_QUERY);
+            preparedStatement.setString(1, user.getUserName());
+            preparedStatement.setString(2, user.getEmail());
+            preparedStatement.setString(3, user.getPassword());
+            preparedStatement.setInt(4, user.getId());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void delete(int userId) {
+        try(Connection connection = DbUtil.getConnection()) {
+            PreparedStatement preparedStatement = connection.prepareStatement(DELETE_USER_QUERY);
+            preparedStatement.setInt(1, userId);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public User[] findAllById(int id) {
+        try(Connection connection = DbUtil.getConnection()) {
+            User[] users = new User[0];
+            PreparedStatement preparedStatement = connection.prepareStatement(FIND_ALL_QUERY);
+            preparedStatement.setInt(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while(resultSet.next()) {
+                User user = new User();
+                user.setUserName(resultSet.getString("username"));
+                user.setEmail(resultSet.getString("email"));
+                user.setPassword(resultSet.getString("password"));
+                users = addToArray(user, users);
+            }
+
+            return users;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            User[] emptyUserList = new User[0];
+            return emptyUserList;
+        }
+    }
+
+    private User[] addToArray(User u, User[] users) {
+        User[] tempArray = Arrays.copyOf(users, users.length + 1);
+        tempArray[users.length] = u;
+        users = tempArray;
+        return users;
+    }
+}
---
+++ src/main/java/project/views/HomeServlet.java
@@ -0,0 +1,25 @@
+//package project.views;
+//
+//import project.dao.UserDao;
+//import project.model.User;
+//
+//import javax.servlet.ServletException;
+//import javax.servlet.annotation.WebServlet;
+//import javax.servlet.http.HttpServlet;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import java.io.IOException;
+//
+//@WebServlet("/")
+//public class HomeServlet extends HttpServlet {
+//    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//
+//    }
+//
+//    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        UserDao userDao = new UserDao();
+//        User user = userDao.read(1);
+//        String message = user.toString();
+//        response.getWriter().append(message);
+//    }
+//}
